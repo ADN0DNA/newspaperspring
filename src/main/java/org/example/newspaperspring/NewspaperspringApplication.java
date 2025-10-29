b23818e5bde54e564f0b6cccb243207648f125d0
ADN__DNA <inspectoradri@gmail.com>
2025-10-29 12:46:26 +0100

    Initial commit

---
+++ src/main/java/org/example/newspaperspring/NewspaperspringApplication.java
@@ -0,0 +1,13 @@
+package org.example.newspaperspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaperspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspaperspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/newspaperspring/NewspaperspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newspaperspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaperspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
